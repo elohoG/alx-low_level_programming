d831d08af57200794e447975a96634d62b9de54c
elohoG <elohogodswill@gmail.com>
2023-10-02 09:44:15 -0700

    correction commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    if (argc > 0) {
-        printf("%s\n", argv[0]);
-    }
-    return 0;
-}
+/**
+* main - entry point
+* @argc: size of argv
+* @argv: array
+* Return: 0
+**/
 
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
